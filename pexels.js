a7d3f54aec5830941ecdfddc9b58fb0bd5746dfe
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-10-12 19:32:20 +0200

    md

--- pexels.js
+++ pexels.js
@@ -1,90 +1,80 @@
-const PEXELS_KEY = 'Hft5gxEJ164U0nzo9UblQrN8lrTG26FboMC1kBE0JhQUSLj8VBOeHkcq'
-// funziona anche con 'ciao'?
+// creiamo la const per la chiave API
+const KEY = 'Hft5gxEJ164U0nzo9UblQrN8lrTG26FboMC1kBE0JhQUSLj8VBOeHkcq'
 
-const getImages = function (query) {
-    fetch('https://api.pexels.com/v1/search?query=' + query, {
-        headers: {
-            authorization: PEXELS_KEY,
-        },
+const getNewImage = function (query) {
+  fetch('https://api.pexels.com/v1/search?query=' + query, {
+    headers: {
+      authorization: KEY,
+    },
+  })
+    .then(res => {
+      if (res.ok) {
+        console.log(res)
+        return res.json()
+      } else {
+        throw new Error('Errore nel response')
+      }
     })
-        .then((res) => {
-            if (res.ok) {
-                return res.json()
-            } else {
-                throw new Error('Errore nella response')
-            }
-        })
-        .then((data) => {
-            console.log(data)
-
-            const row = document.querySelector('#content-row')
-            console.log(row)
-            row.innerHTML = ''
-            data.photos.forEach((photo) => {
-                const newCol = document.createElement('div')
-                newCol.classList.add('col', 'col-md-4')
-                newCol.innerHTML = `
-            <div class="card mb-4 shadow-sm">
+    .then(dataImg => {
+      console.log(dataImg)
+      const row = document.getElementById('row-image')
+      row.innerHTML = ''
+      dataImg.photos.forEach(photo => {
+        const col = document.createElement('div')
+        col.classList.add('col', 'col-md-4')
+        col.innerHTML = ` <div class="card mb-4 shadow-sm">
                 <img
-                src="${photo.src.medium}"
-                class="bd-placeholder-img card-img-top"
+                  src=${photo.src.original}
+                  class="bd-placeholder-img card-img-top"
                 />
                 <div class="card-body">
-                <a href="./details.html?imageId=${photo.id}"><h5 class="card-title">Lorem Ipsum</h5></a>
-                <p class="card-text">
+                  <h5 class="card-title">${photo.photographer}</h5>
+                  <p class="card-text">
                     ${photo.alt}
-                </p>
-                <div
+                  </p>
+                  <div
                     class="d-flex justify-content-between align-items-center"
-                >
+                  >
                     <div class="btn-group">
-                    <button
+                      <button
                         type="button"
                         class="btn btn-sm btn-outline-secondary"
-                    >
+                      >
                         View
-                    </button>
-                    <button
+                      </button>
+                      <button
                         type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                    >
-                        Edit
-                    </button>
+                        class="btn btn-sm btn-outline-secondary" onclick="removeCard(this)"
+                      >
+                        Hide
+                      </button>
                     </div>
                     <small class="text-muted">${photo.id}</small>
+                  </div>
                 </div>
-                </div>
-            </div>
-            `
-                row.appendChild(newCol)
-            })
-            // cambiamo tutti gli edit in hide
-            const allTheEditButtons = document.querySelectorAll(
-                '.card .btn:nth-of-type(2)'
-            )
-            allTheEditButtons.forEach((button) => {
-                button.innerText = 'Hide'
-                button.addEventListener('click', function (e) {
-                    // questo avviene al click di uno qualsiasi dei nuovi bottoni Hide
-                    console.log('click!', e.target)
-                    // cerco, risalendo il DOM, la col che contiene questo bottone
-                    e.target.closest('.col').remove()
-                })
-            })
-        })
-        .catch((err) => console.log(err))
+              </div>`
+        row.appendChild(col)
+      });
+    })
+    .catch(err => console.log(err))
 }
 
-const primaryButton = document.querySelector('.btn-primary')
-primaryButton.addEventListener('click', () => getImages('kittens'))
+const removeCard = function (button) {
+  const card = button.closest(".col-md-4")
+  card.remove()
+}
 
-const secondaryButton = document.querySelector('.btn-secondary')
-secondaryButton.addEventListener('click', () => getImages('hamsters'))
 
-const searchForm = document.getElementById('custom-search')
-searchForm.addEventListener('submit', function (e) {
-    e.preventDefault()
-    const searchBar = document.getElementById('search-field')
-    const searchValue = searchBar.value
-    getImages(searchValue)
+const firstBtn = document.getElementById('first')
+firstBtn.addEventListener('click', () => getNewImage('sea'))
+
+const secondBtn = document.getElementById('second')
+secondBtn.addEventListener('click', () => getNewImage('fish'))
+
+const formSearch = document.getElementById('form')
+formSearch.addEventListener('submit', function (e) {
+  e.preventDefault()
+  const bar = document.getElementById('inputSearch')
+  const search = bar.value
+  getNewImage(search)
 })
